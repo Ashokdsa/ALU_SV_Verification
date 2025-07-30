542dcf30734669cdd21c6a28cbde5e05db5ae94f
Ashokdsa <ashokpiusdsa@gmail.com>
2025-07-30 23:38:32 +0530

    REMOVED excess display statements

--- src/driver.sv
+++ src/driver.sv
@@ -170,17 +170,14 @@ class alu_driver;
           valid_a = flag && trans.inp_valid[0] ? 1'b1 : valid_a;
           valid_b = flag && trans.inp_valid[1] ? 1'b1 : valid_b;
           count3 = (previ == {trans.mode,trans.cmd}) ? count3 : 0;
-        $display("\n%0t | DRV: FLAG = %0b VALID_A = %0b VALID_B = %0b",$time/10 - 1,flag,valid_a,valid_b);
         if(flag && count3 > 0) begin
           count3++;
           if(count3 >= 4)
             count3 = 0;
           repeat(2)@(vif.drv_cb);
-          $display(" %0t | DRV: ENTERED MULTIPLICATION COUNT = %0d",$time/10 - 1,count3);
         end
         else if(flag == 0)
         begin
-          $display("%0t | DRV: FLAG ZERO GAVE ONE DELAY",$time/10 - 1);
           count = 0;
           repeat(1)@(vif.drv_cb);
         end
@@ -195,21 +192,18 @@ class alu_driver;
           end
           repeat(1)@(vif.drv_cb);
           count = 0;
-          $display("%0t | DRV: BOTH VALID FLAG",$time/10 - 1);
         end
         else if(count < 16 && flag)
         begin
           count++;
           if(count == 0)
             repeat(1)@(vif.drv_cb);
-          $display("%0t | DRV: COUNTING = %0d",$time/10 - 1,count);
         end
         else if(count >= 16) begin
           valid_a = 0;
           valid_b = 0;
           count = 0;
           flag = 0;
-          $display("%0t | DRV: COUNT EXCEEDED",$time/10 - 1);
         end
         previ = {trans.mode,trans.cmd};
         end
@@ -229,56 +223,5 @@ class alu_driver;
     $display("DRV: COMPLETED %0d ITERATIONS",i);
     end
     alu_input_cg.stop;
-  endtask
-endclass
-
-class driv_same extends alu_driver;
-  function new(mailbox #(base_transaction) mbx_gen_drv,mailbox #(base_transaction) mbx_drv_ref,virtual alu_intf.DRV vif);
-    super.new(mbx_gen_drv,mbx_drv_ref,vif);
-  endfunction
-  task start(int no);
-    repeat(1)@(vif.drv_cb);
-    repeat(no)
-    begin
-      trans = new();
-      @(vif.drv_cb);
-      mbx_gen_drv.get(trans);
-      if(trans.rst == 1)
-      begin
-        vif.drv_cb.rst <= 1;
-        vif.drv_cb.inp_valid<= 2'b00;
-        vif.drv_cb.mode<= 0;
-        vif.drv_cb.cmd<= 0;
-        vif.drv_cb.ce<= 1;
-        vif.drv_cb.opa<= 0;
-        vif.drv_cb.opb<= 0;
-        vif.drv_cb.cin<= 0;
-      end
-      else
-      begin
-        vif.drv_cb.rst <= 0;
-        vif.drv_cb.inp_valid<= trans.inp_valid;
-        vif.drv_cb.mode<= trans.mode;
-        vif.drv_cb.cmd<= trans.cmd;
-        vif.drv_cb.ce <= trans.ce;
-        vif.drv_cb.opa <= trans.opa;
-        vif.drv_cb.opb <= trans.opb;
-        vif.drv_cb.cin <= trans.cin;
-      end
-      repeat(1)@(vif.drv_cb);
-      if(i == 0)
-        repeat(1)@(vif.drv_cb);
-      mbx_drv_ref.put(trans);
-      i = i + 1;
-      $display("----------------------%0d---------------------\nDRV: DRIVEN THE VALUES TO DUT",i);
-      if(trans.mode)
-        $display("RST = %0b CE = %1b\nINP_VALID = %2b MODE = %1b CMD = %0s\nOPA = %0d OPB = %0d CIN = %0b",trans.rst,trans.ce,trans.inp_valid,trans.mode,arith'(trans.cmd),trans.opa,trans.opb,trans.cin);
-      else
-      $display("RST = %0b CE = %1b\nINP_VALID = %2b MODE = %1b CMD = %0s\nOPA = %b\nOPB = %b",trans.rst,trans.ce,trans.inp_valid,trans.mode,logical'(trans.cmd),trans.opa,trans.opb);
-    repeat(3)@(vif.drv_cb);
-    mbx_drv_ref.put(trans);
-    $display("RAN IT FOR ONE MORE CYCLE");
-    end
-    $display("DRV: COMPLETED %0d ITERATIONS",i);
   endtask
 endclass
